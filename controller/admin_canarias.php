66afdd3cef003344b997ac6138f2a012dc3df0f7
Francesc Pineda Segarra <shawe.ewahs@gmail.com>
2017-04-20 09:08:19 +0200

    - Solucionados reportes de Carlos: https://www.facturascripts.com/comm3/index.php?page=community_item&id=4976 - Se cambiaban las longitudes de subcuentas a 6 digitos, deben ser 10 - Añadido carga de páginas al Wizard, no se cargaba el de libros de impuestos - Pequeños cambios en el asistente para mejorar la usabilidad

--- controller/admin_canarias.php
+++ controller/admin_canarias.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of FacturaSctipts
+ * This file is part of FacturaScripts
  * Copyright (C) 2017  Francesc Pineda Segarra  francesc.pineda@x-netdigital.com
  *
  * This program is free software: you can redistribute it and/or modify
@@ -27,90 +27,42 @@ require_model('subcuenta.php');
  *
  * @author Francesc Pineda Segarra
  */
-class admin_canarias extends fs_controller
-{
-   public function __construct()
-   {
-      parent::__construct(__CLASS__, 'Canarias', 'admin');
-   }
-   
-   protected function private_core()
-   {
-      if( isset($_GET['opcion']) )
-      {
-         if($_GET['opcion'] == 'moneda')
-         {
-            $this->empresa->coddivisa = 'EUR';
-            if( $this->empresa->save() )
-            {
-               $this->new_message('Datos guardados correctamente.');
-            }
-         }
-         else if($_GET['opcion'] == 'pais')
-         {
-            $this->empresa->codpais = 'ESP';
-            if( $this->empresa->save() )
-            {
-               $this->new_message('Datos guardados correctamente.');
-            }
-         }
-         else if($_GET['opcion'] == 'regimenes')
-         {
-            $fsvar = new fs_var();
-            if( $fsvar->simple_save('cliente::regimenes_iva', 'Simplificado,Común,Exento') )
-            {
-               $this->new_message('Datos guardados correctamente.');
-            }
-         }
-         else if($_GET['opcion'] == 'impuestos')
-         {
-            $this->set_impuestos();
-         }
-      }
-      else
-      {
-         $this->check_ejercicio();
-         $this->share_extensions();
-         
+class admin_canarias extends fs_controller {
 
-         /// ¿Guardamos las opciones avanzadas?
-         $guardar = FALSE;
-         foreach($GLOBALS['config2'] as $i => $value)
-         {
-            if( isset($_POST[$i]) )
-            {
-               $GLOBALS['config2'][$i] = $_POST[$i];
-               $guardar = TRUE;
+   public function __construct() {
+      parent::__construct(__CLASS__, 'Canarias', 'admin');
+      $this->share_extensions();
+   }
+
+   protected function private_core() {
+      if (isset($_GET['opcion'])) {
+         if ($_GET['opcion'] == 'moneda') {
+            $this->empresa->coddivisa = 'EUR';
+            if ($this->empresa->save()) {
+               $this->new_message('Datos guardados correctamente.');
             }
-         }
-         
-         if($guardar)
-         {
-            $file = fopen('tmp/'.FS_TMP_NAME.'config2.ini', 'w');
-            if($file)
-            {
-               foreach($GLOBALS['config2'] as $i => $value)
-               {
-                  if( is_numeric($value) )
-                  {
-                     fwrite($file, $i." = ".$value.";\n");
-                  }
-                  else
-                  {
-                     fwrite($file, $i." = '".$value."';\n");
-                  }
-               }
-               
-               fclose($file);
+         } else if ($_GET['opcion'] == 'pais') {
+            $this->empresa->codpais = 'ESP';
+            if ($this->empresa->save()) {
+               $this->new_message('Datos guardados correctamente.');
             }
-            
-            $this->new_message('Datos de traducciones guardados correctamente.');
+         } else if ($_GET['opcion'] == 'regimenes') {
+            $fsvar = new fs_var();
+            if ($fsvar->simple_save('cliente::regimenes_iva', 'Simplificado,Común,Exento')) {
+               $this->new_message('Datos guardados correctamente.');
+            }
+         } else if ($_GET['opcion'] == 'impuestos') {
+            $this->set_impuestos();
+         } else if ($_GET['opcion'] == 'actualizar_config') {
+            $this->actualizar_config2();
          }
+      } else {
+         $this->check_menu();
+         $this->check_ejercicio();
       }
    }
-   
-   private function share_extensions()
-   {
+
+   private function share_extensions() {
       $fsext = new fs_extension();
       $fsext->name = 'impuestos_canarias';
       $fsext->from = __CLASS__;
@@ -120,96 +72,105 @@ class admin_canarias extends fs_controller
       $fsext->params = 'plugins/canarias/extras/canarias.xml';
       $fsext->save();
    }
-   
-   private function check_ejercicio()
-   {
-      $ej0 = new ejercicio();
-      foreach($ej0->all_abiertos() as $ejercicio)
-      {
-         if($ejercicio->longsubcuenta != 6)
-         {
-            $ejercicio->longsubcuenta = 6;
-            if( $ejercicio->save() )
-            {
-               $this->new_message('Datos del ejercicio '.$ejercicio->codejercicio.' modificados correctamente.');
+
+   private function check_menu() {
+
+      // Limpiamos la cache por si ha habido cambio en la estructura de las tablas
+      $this->cache->clean();
+
+      if (file_exists(__DIR__)) {
+         /// activamos las páginas del plugin
+         foreach (scandir(__DIR__) as $f) {
+            if (is_string($f) AND strlen($f) > 0 AND ! is_dir($f) AND $f != __CLASS__ . '.php') {
+               $page_name = substr($f, 0, -4);
+
+               require_once __DIR__ . '/' . $f;
+               $new_fsc = new $page_name();
+
+               if (!$new_fsc->page->save()) {
+                  $this->new_error_msg("Imposible guardar la página " . $page_name);
+               }
+
+               unset($new_fsc);
             }
-            else
-            {
+         }
+      } else {
+         $this->new_error_msg('No se encuentra el directorio ' . __DIR__);
+      }
+
+      $this->load_menu(TRUE);
+   }
+
+   private function check_ejercicio() {
+      $ej0 = new ejercicio();
+      foreach ($ej0->all_abiertos() as $ejercicio) {
+         if ($ejercicio->longsubcuenta != 10) {
+            $ejercicio->longsubcuenta = 10;
+            if ($ejercicio->save()) {
+               $this->new_message('Datos del ejercicio ' . $ejercicio->codejercicio . ' modificados correctamente.');
+            } else {
                $this->new_error_msg('Error al modificar el ejercicio.');
             }
          }
       }
    }
-   
-   public function regimenes_ok()
-   {
+
+   public function regimenes_ok() {
       $fsvar = new fs_var();
       $regimenes = $fsvar->simple_get('cliente::regimenes_iva');
-      
-      if($regimenes == 'Simplificado,Común,Exento')
-      {
+
+      if ($regimenes == 'Simplificado,Común,Exento') {
          return TRUE;
-      }
-      else
-      {
+      } else {
          return FALSE;
       }
    }
-   
-   public function ejercicio_ok()
-   {
+
+   public function ejercicio_ok() {
       $ok = FALSE;
-      
+
       $ej0 = new ejercicio();
-      $ejerccio = $ej0->get_by_fecha( $this->today() );
-      if($ejerccio)
-      {
+      $ejerccio = $ej0->get_by_fecha($this->today());
+      if ($ejerccio) {
          $subc0 = new subcuenta();
-         foreach($subc0->all_from_ejercicio($ejerccio->codejercicio) as $sc)
-         {
+         foreach ($subc0->all_from_ejercicio($ejerccio->codejercicio) as $sc) {
             $ok = TRUE;
             break;
          }
       }
-      
+
       return $ok;
    }
-   
-   public function impuestos_ok()
-   {
+
+   public function impuestos_ok() {
       $ok = FALSE;
-      
+
       $imp0 = new impuesto();
-      foreach($imp0->all() as $i)
-      {
-         if($i->codimpuesto == 'IGIC7')
-         {
+      foreach ($imp0->all() as $i) {
+         if ($i->codimpuesto == 'IGIC7') {
             $ok = TRUE;
             break;
          }
       }
-      
+
       return $ok;
    }
-   
-   private function set_impuestos()
-   {
+
+   private function set_impuestos() {
       /// eliminamos los impuestos que ya existen (los de España)
       $imp0 = new impuesto();
-      foreach($imp0->all() as $impuesto)
-      {
+      foreach ($imp0->all() as $impuesto) {
          $this->desvincular_articulos($impuesto->codimpuesto);
          $impuesto->delete();
       }
-      
+
       /// añadimos los de Canarias
-      $codimp = array("IGIC7","IGIC3","IGIC0");
-      $desc = array("IGIC 7%","IGIC 3%","IGIC 0%");
+      $codimp = array("IGIC7", "IGIC3", "IGIC0");
+      $desc = array("IGIC 7%", "IGIC 3%", "IGIC 0%");
       $recargo = 0;
       $iva = array(7, 3, 0);
       $cant = count($codimp);
-      for($i=0; $i<$cant; $i++)
-      {
+      for ($i = 0; $i < $cant; $i++) {
          $impuesto = new impuesto();
          $impuesto->codimpuesto = $codimp[$i];
          $impuesto->descripcion = $desc[$i];
@@ -217,31 +178,65 @@ class admin_canarias extends fs_controller
          $impuesto->iva = $iva[$i];
          $impuesto->save();
       }
-      
+
       $this->impuestos_ok = TRUE;
       $this->new_message('Impuestos de Canarias añadidos.');
    }
-   
-   private function desvincular_articulos($codimpuesto)
-   {
+
+   private function desvincular_articulos($codimpuesto) {
       $sql = "UPDATE articulos SET codimpuesto = null WHERE codimpuesto = "
-              .$this->empresa->var2str($codimpuesto).';';
-      
-      if( $this->db->table_exists('articulos') )
-      {
+              . $this->empresa->var2str($codimpuesto) . ';';
+
+      if ($this->db->table_exists('articulos')) {
          $this->db->exec($sql);
       }
    }
-   
-   public function formato_divisa_ok()
-   {
-      if(FS_POS_DIVISA == 'right')
-      {
+
+   public function formato_divisa_ok() {
+      if (FS_POS_DIVISA == 'right') {
          return TRUE;
-      }
-      else
-      {
+      } else {
          return FALSE;
       }
    }
+
+   public function nombre_impuesto_ok() {
+      if ($GLOBALS['config2']['iva'] == 'IGIC') {
+         return TRUE;
+      } else {
+         return FALSE;
+      }
+   }
+
+   public function actualizar_config2() {
+      //Configuramos la información básica para config2.ini
+      $guardar = FALSE;
+      $config2 = array();
+      /* No hace falta indicarlas todas, sólo las diferentes */
+      $config2['zona_horaria'] = "Atlantic/Canary";
+      $config2['iva'] = "IGIC";
+
+      foreach ($GLOBALS['config2'] as $i => $value) {
+         if (isset($config2[$i])) {
+            $GLOBALS['config2'][$i] = htmlspecialchars($config2[$i]);
+            $guardar = TRUE;
+         }
+      }
+
+      if ($guardar) {
+         $file = fopen('tmp/' . FS_TMP_NAME . 'config2.ini', 'w');
+         if ($file) {
+            foreach ($GLOBALS['config2'] as $i => $value) {
+               if (is_numeric($value)) {
+                  fwrite($file, $i . " = " . $value . ";\n");
+               } else {
+                  fwrite($file, $i . " = '" . $value . "';\n");
+               }
+            }
+            fclose($file);
+         }
+         $this->new_message('Datos de configuracion regional guardados correctamente.');
+      }
+   }
+
 }
